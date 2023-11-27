969ee965d98ff5b4c2f2b4b0aecf29c9ddc44db1
Ricardo Goncalves Pereira <ricardo.pereira@luizalabs.com>
2023-11-27 16:56:04 -0300

    feat:add pocSSE project

---
+++ api/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/values', (_req, res, _next) => {
+  res.set({
+    'Access-Control-Allow-Origin': '*',
+    'Cache-Control': 'no-cache',
+    Connection: 'keep-alive',
+    'Content-Type': 'text/event-stream',
+  });
+  res.flushHeaders();
+
+  const interval = setInterval(() => {
+    const value1Rate = Math.floor(Math.random() * 100000);
+    const value2Rate = Math.floor(Math.random() * 60000);
+    res.write(`data: ${JSON.stringify({ value1Rate, value2Rate })}\n\n`);
+  }, 2000);
+
+  res.on('close', () => {
+    clearInterval(interval);
+    res.end();
+  });
+});
+
+app.listen(4001, 'localhost', () => {
+  console.log('Server is up and running at port 4001');
+});
